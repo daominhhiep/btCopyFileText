c69b9bee78151bc840940a66608c8583a4a96df1
admin <daominhhiep97@gmail.com>
2020-07-27 15:06:59 +0700

    [Bài tập] Copy file text

--- src/CopyFileExample.java
+++ src/CopyFileExample.java
@@ -13,16 +13,18 @@ public class CopyFileExample {
         try {
             inStream = new FileInputStream(new File("./txt/file1.txt"));
             outStream = new FileOutputStream(new File("./txt/file2.txt"));
-
+            int count = 0;
             int length;
             byte[] buffer = new byte[1024];
 
             while ((length = inStream.read(buffer)) > 0) {
                 outStream.write(buffer, 0, length);
+                count+=length;
             }
-            System.out.println("File is copied successful!");
+            System.out.println("File is copied successful!" );
+            System.out.println("Number of characters in file : " + count);
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("Can't find file1.txt");
         } finally {
             inStream.close();
             outStream.close();
